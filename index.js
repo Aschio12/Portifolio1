b0e3559ad68aeecb9816dbf3b4110ab3338d5281
Aschalew Daraje <aschalewdere2@gmail.com>
2025-03-10 05:37:27 -0700

    Project sample
    The required functionalties for the project sample part is aplied well with same styling and a little change on the html for this project sample part

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-import image1 from "./Images/burger-menu.png";
-import image2 from "./Images/lines.png";
-$(document).ready(function() {
+// No imports needed since we're not using a bundler
+$(document).ready(function () {
+    let currentIndex = 0;
     const projects = [
-        { address: image1, description: "This is the description of project 1" },
-        { address: image2, description: "This is the description of project 2" },
+        { address: "./Images/burger-menu.png", description: "This is the description of project 1" },
+        { address: "./Images/lines.png", description: "This is the description of project 2" },
     ];
 
-    const left = $("#left-btn");
-    const description = $("#description"); 
-    const forDetail = $("#for-detail"); 
-    const projectImage = $("#project-image"); 
+    const leftBtn = $("#left-btn");
+    const description = $("#description");
+    const projectImage = $("#project-image");
     const rightBtn = $("#right-btn");
 
-    
+    // Initial display
+    description.text(projects[currentIndex].description);
+    projectImage.css("background-image", `url(${projects[currentIndex].address})`);
 
+    function updateDisplay() {
+        description.text(projects[currentIndex].description);
+        projectImage.css("background-image", `url(${projects[currentIndex].address})`);
+    }
+
+    // Right button click
+    rightBtn.click(function () {
+        if (currentIndex < projects.length - 1) {
+            currentIndex += 1;    
+        } else {
+            currentIndex = 0;
+        }
+        updateDisplay();
+    });
+
+    // Left button click
+    leftBtn.click(function () {
+        if (currentIndex > 0) {
+            currentIndex -= 1;
+        } else {
+            currentIndex = projects.length - 1;
+        }
+        updateDisplay();
+    });
 });
