734f58018b10873110c5b61cfcb7526b72658f12
Aschalew Daraje <aschalewdere2@gmail.com>
2025-03-10 04:38:07 -0700

    Project sample
    The required varibales are declared,the required image sample are imported,and the requred function are declared in the htm file using onclick attribute in

---
+++ index.js
@@ -0,0 +1,17 @@
+import image1 from "./Images/burger-menu.png";
+import image2 from "./Images/lines.png";
+$(document).ready(function() {
+    const projects = [
+        { address: image1, description: "This is the description of project 1" },
+        { address: image2, description: "This is the description of project 2" },
+    ];
+
+    const left = $("#left-btn");
+    const description = $("#description"); 
+    const forDetail = $("#for-detail"); 
+    const projectImage = $("#project-image"); 
+    const rightBtn = $("#right-btn");
+
+    
+
+});
